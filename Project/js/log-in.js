ad6b2109bb8bcbc9a89e11ba0cdd1f4402138b31
Shawky Ebrahim Ahmed <101745968+shawkyebrahim2514@users.noreply.github.com>
2023-04-16 22:31:34 +0200

    Update admin-panel and login page

--- Project/js/log-in.js
+++ Project/js/log-in.js
@@ -1,11 +1,37 @@
 //get Data
-const idNumberInput = document.getElementById('ID');
-const passwordInput = document.getElementById('Password');
+if (window.localStorage.getItem("admins") == null) {
+  let admins = {
+    20210184: {
+      id: "20210184",
+      password: "admin",
+      firstName: "Shawky",
+      lastName: "Ebrahim",
+    },
+    20210755: {
+      id: "20210755",
+      password: "admin",
+      firstName: "Alan",
+      lastName: "Samir",
+    },
+  };
+  window.localStorage.setItem("admins", JSON.stringify(admins));
+}
 
-let allStudents = JSON.parse(window.localStorage.getItem('studentInfo'));
-for(ids in allStudents){
-    if(ids == idNumberInput){
-        let zData = JSON.parse(localStorage.getItem('studentInfo'))[ids];
-        if(zData[''])
+if(window.localStorage.getItem("loggedAdmin") != null){
+    window.location.href = "../admin-panel.html";
+}
+
+function loginAdmin() {
+  const idNumberInput = document.getElementById("ID").value;
+  const passwordInput = document.getElementById("Password").value;
+  let allAdmins = JSON.parse(window.localStorage.getItem("admins"));
+  if (allAdmins[idNumberInput]) {
+    if (allAdmins[idNumberInput].password == passwordInput) {
+      window.localStorage.setItem(
+        "loggedAdmin",
+        JSON.stringify(allAdmins[idNumberInput])
+      );
+      window.location.href = "../admin-panel.html";
     }
+  }
 }
