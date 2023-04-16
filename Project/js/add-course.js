48b5fa0921e13f4a498f633f8ea43ec8ce6ed3f3
Shawky Ebrahim Ahmed <101745968+shawkyebrahim2514@users.noreply.github.com>
2023-04-17 00:51:16 +0200

    Update admin controllers messages

--- Project/js/add-course.js
+++ Project/js/add-course.js
@@ -1,19 +1,57 @@
-function loadAllDepartments() {
-  let departmentDropList = document.getElementById('department')
-  let options = '<option value"">Choose Department</option>';
-  let allDepartments = JSON.parse(window.localStorage.getItem('allDepartments'))
-  for (let department in allDepartments) {
-    options += '<option value="' + department + '">' + department + '</option>'
-  }
-  departmentDropList.innerHTML = options
+if (!window.localStorage.getItem("allDepartments")) {
+  window.localStorage.setItem("allDepartments", "{}");
 }
 
-loadAllDepartments()
+function loadAllDepartments() {
+  let departmentDropList = document.getElementById("department");
+  let options = '<option value"">Choose Department</option>';
+  let allDepartments = JSON.parse(
+    window.localStorage.getItem("allDepartments")
+  );
+  for (let department in allDepartments) {
+    options += '<option value="' + department + '">' + department + "</option>";
+  }
+  departmentDropList.innerHTML = options;
+}
+
+loadAllDepartments();
 
 function addNewCourse() {
-  let departmentName = document.getElementById('department').value
-  let newCourseName = document.getElementsByName('courseName')[0].value
-  let allDepartments = JSON.parse(window.localStorage.getItem('allDepartments'))
-  allDepartments[departmentName].push(newCourseName)
-  window.localStorage.setItem('allDepartments', JSON.stringify(allDepartments))
+  let departmentName = document.getElementById("department").value;
+  if (!departmentName) {
+    failedToAdd("Please choose a department");
+    return;
+  }
+  let newCourseName = document.getElementsByName("courseName")[0].value;
+  if (!newCourseName) {
+    failedToAdd("Please enter a course name");
+    return;
+  }
+  let allDepartments = JSON.parse(
+    window.localStorage.getItem("allDepartments")
+  );
+  if (allDepartments[departmentName].includes(newCourseName)) {
+    failedToAdd(
+      `The Course (${newCourseName}) is already exist, Please try again`
+    );
+    return;
+  }
+  allDepartments[departmentName].push(newCourseName);
+  window.localStorage.setItem("allDepartments", JSON.stringify(allDepartments));
+  successfullyAdding();
+}
+
+function failedToAdd(paragraph) {
+  window.localStorage.setItem("warning", paragraph);
+  window.location.href = "add-course.html";
+}
+
+function successfullyAdding() {
+  window.localStorage.setItem(
+    "successful",
+    `Successfully Added Course (${
+      document.getElementsByName("courseName")[0].value
+    })`
+  );
+  window.location.href = "add-course.html";
 }
