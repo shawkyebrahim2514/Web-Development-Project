8c228ea8c465236c7ce76ffc43e09f5d956f38bc
Shawky Ebrahim Ahmed <101745968+shawkyebrahim2514@users.noreply.github.com>
2023-04-17 12:54:32 +0200

    Updateing navbar and add (add student) section

--- Project/js/nav-bar.js
+++ Project/js/nav-bar.js
@@ -14,11 +14,11 @@ function showNavBar(){
     <div class="links">
       <ul>
         <li class="main-list"><a href="admin-panel.html"> Home </a></li>
-        <li class="main-list"><a href="all-students-page.html">View All</a></li>
         <li class="main-list"><a href="add-department.html">Add department</a></li>
         <li class="main-list"><a href="add-course.html">Add Course</a></li>
+        <li class="main-list"><a href="registration.html">Add Student</a></li>
+        <li class="main-list"><a href="all-students-page.html">View All</a></li>
         <li class="main-list"><a href="student-page.html">Search</a></li>
-        
       </ul>
     </div>
   </div>`
